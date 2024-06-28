456134cb7c722ae126de35edfd0f9ce1ce41fdc2
Anirudh Agarwal <aagarwal7_be21@thapar.edu>
2024-06-28 17:16:00 +0530

    centered all the components and beautified app.js

---
+++ src/components/StartApplicationButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function StartApplicationButton() {
+  return (
+    <div>
+      <div className="container">
+        <button type="button" class="btn btn-primary btn-lg my-5" disabled>
+          Start Application
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default StartApplicationButton;
---
+++ src/pages/PersonalDetailsPage.jsx
@@ -0,0 +1,9 @@
+function PersonalDetailsPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PersonalDetailsPage;
