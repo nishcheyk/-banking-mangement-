11e0587de35d15c554f67d39ec4a301246ddf076
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-07-16 13:59:39 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route,Routes,Router } from 'react-router-dom';
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import EditProfile from "./pages/EditProfile";
-require("dotenv").config();
-const root = ReactDOM.createRoot(document.getElementById("root"));
+require('dotenv').config();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Registeration" element={<Register />} />
-        <Route path="/EditProfile" element={<EditProfile />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Registeration" element={<Register />} />
+          <Route path="/EditProfile" element={<EditProfile />} />
+        </Routes>
+      </ BrowserRouter>
     </AuthProvider>
-  </BrowserRouter>
+  </React.StrictMode>
 );
 reportWebVitals();
