869963069fe1db4115e2d9d49e1e814d5fcc2ccc
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-07-16 14:06:12 +0530

    tessst

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route,Routes,Router } from 'react-router-dom';
 
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import EditProfile from "./pages/EditProfile";
-require("dotenv").config();
-const root = ReactDOM.createRoot(document.getElementById("root"));
+require('dotenv').config();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Registeration" element={<Register />} />
-        <Route path="/EditProfile" element={<EditProfile />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Registeration" element={<Register />} />
+          <Route path="/EditProfile" element={<EditProfile />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+
 );
 reportWebVitals();
