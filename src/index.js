5f32aef14f7273743187ccff5c932b7f5d039fc7
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-07-16 13:43:22 +0530

    index.css removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
